8dbbac72f3bff6d9ce63ee9c23b4261821aa5682
bethbarlow7 <bethbarlow7@gmail.com>
2023-02-27 20:03:41 +0000

    moves all preprocessing to EDA.ipynb

--- utils/.ipynb_checkpoints/helpers-checkpoint.py
+++ utils/.ipynb_checkpoints/helpers-checkpoint.py
@@ -1,80 +1,90 @@
-import matplotlib.pyplot as plt
+# Import libraries
+import time
+
+import numpy as np
+import optuna
 import pandas as pd
+from sklearn import metrics
+from sklearn.metrics import (
+    classification_report,
+    f1_score
+)
+
+from xgboost import XGBClassifier
+
+optuna.logging.set_verbosity(optuna.logging.WARNING)
+
+import matplotlib.pyplot as plt
 import seaborn as sns
 
-def fit_model_get_predictions(classifier, features):
-    
+
+def fit_model_get_results(classifier, features, X_train, y_train, X_val, y_val):
+
     """
-    Fits a model and gets predictions and execution times for train and test sets
+    Fits a model and gets results and execution times for train and test sets
+    
+    Inputs:
+    classifier - defined model object
+    features - list of features
+    X_train - training feature set
+    y_train - training labels
+    X_val - validation feature set
+    y_val - validation labels
+    
+    Outputs:
+    model_results_dictionary - dictionary containing model object, f1-scores and execution times for training and predictions
+    
     """
     start_time = time.time()
-    
+
     trained_model = classifier.fit(X_train, y_train)
     training_execution_time = time.time() - start_time
-                              
+
     trained_model.feature_names = features
-    
+
     # get predictions
     train_preds = classifier.predict(X_train)
     train_classification = classification_report(y_train, train_preds)
-    
-    train_f1 = f1_score(y_train, train_preds, average = 'weighted')
+
+    train_f1 = f1_score(y_train, train_preds, average="weighted")
 
     start_time = time.time()
     val_preds = classifier.predict(X_val)
     prediction_execution_time = time.time() - start_time
-    
-    val_f1 = f1_score(y_val, val_preds, average = 'weighted')
-    
+
+    val_f1 = f1_score(y_val, val_preds, average="weighted")
+
     # get model and predictions dictionary
-    model_and_predictions_dictionary = {
-        
-        'classifier': classifier,
-        'train_f1': train_f1,
-        'val_f1': val_f1,
-        'training_execution_time': np.round(training_execution_time, 0),
-        'prediction_execution_time': np.round(prediction_execution_time, 0)
-        
+    model_results_dictionary = {
+        "classifier": classifier,
+        "train_f1": train_f1,
+        "val_f1": val_f1,
+        "training_execution_time": np.round(training_execution_time, 0),
+        "prediction_execution_time": np.round(prediction_execution_time, 0),
     }
-    
-    return model_and_predictions_dictionary
 
-def objective(trial):
+    return model_results_dictionary
+
+
+def plot_feature_importances(classifier, title, features):
     
     """
-    Defines objective function for optuna to maximise.
-    Takes a trial object as input and returns score
+    Plots feature importances
+    
+    Inputs: 
+    classifier - model object
+    title - name of feature
+    features - list of features
+    
+    Outputs:
+    bar plot of features and feature importances
+    
     """
-
-    params = {
-        'max_depth': trial.suggest_int('max_depth', 1, 9),
-        'learning_rate': trial.suggest_float('learning_rate', 0.01, 1.0),
-        'n_estimators': trial.suggest_int('n_estimators', 50, 500, 50),
-        'min_child_weight': trial.suggest_int('min_child_weight', 1, 10),
-        'subsample': trial.suggest_float('subsample', 0.01, 1.0),
-        'colsample_bytree': trial.suggest_float('colsample_bytree', 0.01, 1.0),
-        'eval_metric': 'auc',
-    }
     
-    clf = XGBClassifier(**params)
-        
-    start_time = time.time()
-    clf.fit(X_train_res, y_train_res)
-    training_execution_time = time.time() - start_time
-    
-    preds = np.rint(clf.predict(X_val))
-    val_df['predictions'] = preds
-        
-    f1 = metrics.f1_score(y_val, preds, average = 'micro')
-    
-    return f1
-
-
-def plot_feature_importances(model, title, features):
 
     feature_importances_df = pd.DataFrame(
         {
-            "Feature Importance": model.feature_importances_,
+            "Feature Importance": classifier.feature_importances_,
             "Feature Name": [v for v in features],
         }
     )
